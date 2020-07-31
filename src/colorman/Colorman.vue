ce00dbb2cc4bc849fa8eba446f514ce2e6ffd688
Daniel Kim <danielsoohwankim@gmail.com>
2020-07-30 20:40:54 -0700

    Colorman app url redirect

---
+++ src/colorman/Colorman.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Prop, Component } from 'vue-property-decorator';
+import detect from 'detect.js';
+
+@Component
+export default class Colorman extends Vue {
+  public mounted(): void {
+    const ua = detect.parse(navigator.userAgent);
+    const os: string = (ua.os.family && ua.os.family !== null) ? ua.os.family.toLowerCase() : '';
+    let url: string = 'https://play.google.com/store/apps/details?id=com.GoldStone.Colorman';
+    // tslint:disable-next-line
+    console.log(`Operating System: ${os}`);
+
+    if (os.includes('ios') === true ||
+        os.includes('mac') === true) {
+      url = 'https://apps.apple.com/us/app/id1523943271';
+    }
+
+    window.location.href = url;
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+</style>
